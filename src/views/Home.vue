d24a047adc6a5b0bd817fd83c7fa2fa9ffd47d51
Zack Borges <zackartborges@gmail.com>
2021-03-24 14:43:05 -0400

    adds axios request for products from simple capstone rails app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="product in products" v-bind:key="product">
+      <h3>{{ product }}</h3>
+    </div>
   </div>
 </template>
 <style></style>
 <script>
+import axios from "axios";
 export default {
-  data: function() {
+  data: function () {
     return {
       message: "Welcome to Vue.js!",
-      
+      products: [],
     };
   },
-  created: function() {},
-  methods: {}
+  created: function () {
+    this.indexProducts();
+  },
+  methods: {
+    indexProducts: function () {
+      axios.get("/api/products").then((response) => {
+        this.products = response.data;
+        console.log("all products:", this.products);
+      });
+    },
+  },
 };
 </script>
